8b4ff93c323ff641b9a900d740820194aad547f2
Rafael A. Muñoz <ramunoz.19@est.ucab.edu.ve>
2023-01-10 05:16:29 -0400

    Frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Button } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> UCAB Rides Chat </h1>
+      <Button colorScheme='blue'>Este es un boton</Button>
+    </div>
+  );
+}
+
+export default App;
